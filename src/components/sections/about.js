11f4c9d57b92754ca6a04cefc3b7b5c7dd355702
rrosenbaum <rrosenbaum4310@gmail.com>
2022-04-01 18:14:00 -0400

    First commit, template works

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -136,10 +136,10 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Ryan Rosenbaum and I enjoy creating things that live on the
+              internet. My interest in web development started back in 2012 when I decided to try
+              editing custom Tumblr themes — turns out hacking together a custom reblog button
+              taught me a lot about HTML &amp; CSS!
             </p>
 
             <p>
