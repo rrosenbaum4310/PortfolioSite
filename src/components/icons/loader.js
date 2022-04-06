95f26fa2a523719d0efd1c320603a472240af9b5
rrosenbaum <rrosenbaum4310@gmail.com>
2022-04-06 14:23:15 -0400

    Fixed the favicon situation on the site. Now have an SVG version of Polygon traced 'R'

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -4,8 +4,11 @@ const IconLoader = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <title>Loader Logo</title>
     <g>
-      <g id="R" transform="translate(11.000000, 5.000000)">
-        <path d="M 10 10 L 10 90 Z L 10 10 A 7 5 -9 1 1 10 53 L 50 90" fill="currentColor" />
+      <g transform="translate(5, -28)">
+        <path
+          fill="currentColor"
+          d="M35 72h4q6 0 9-2t3-6q0-5-3-6t-9-2h-4v16zm0 10v23H23V45h17q12 0 18 5t6 13q0 6-3 10t-8 6l17 26H56L42 82h-7z"
+        />
       </g>
       <path
         stroke="currentColor"
--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 100 100">
     <title>Logo</title>
     <g transform="translate(-8.000000, -2.000000)">
       <g transform="translate(11.000000, 5.000000)">
-        <path d="M 10 10 L 10 90 Z L 10 10 A 7 5 -9 1 1 10 53 L 50 90" fill="currentColor" />
         <polygon
           id="Shape"
           stroke="currentColor"
@@ -15,6 +14,12 @@ const IconLogo = () => (
           points="39 0 0 22 0 67 39 90 78 68 78 23"
         />
       </g>
+    </g>
+    <g transform="translate(0, -28)">
+      <path
+        fill="currentColor"
+        d="M35 72h4q6 0 9-2t3-6q0-5-3-6t-9-2h-4v16zm0 10v23H23V45h17q12 0 18 5t6 13q0 6-3 10t-8 6l17 26H56L42 82h-7z"
+      />
     </g>
   </svg>
 );
